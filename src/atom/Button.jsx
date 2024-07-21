ffdf304807b8222d182246ac2d2a9ae1bb6a2816
neungdong <mjkim9503@naver.com>
2024-07-21 23:42:02 +0900

    UI : Card 와 Button 컴포넌트를 아토믹하게 생성

---
+++ src/atom/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Button = React.forwardRef(({ className, ...props }, ref) => (
+  <ButtonStyled ref={ref} className={className} {...props} />
+));
+Button.displayName = "Button";
+
+Button.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Button;
+
+const ButtonStyled = styled.button`
+  padding: 0.5rem 1rem;
+  background-color: #3b82f6;
+  color: #ffffff;
+  border-radius: 0.5rem;
+  &:hover {
+    background-color: #2563eb;
+  }
+`;
